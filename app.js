fe717cc060083c55634fc7b35086499752560fa1
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-03-16 17:38:59 +0530

    reversed order

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+// 3.
+
+
+
+// 2. Very simple, given an integer or a floating-point number, find its opposite.
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+  
+function opposite(number) {
+    //your code here
+    if(number){
+      return ( number * (-1) );
+    }else {
+      return 0;
+    }
+  }
+
 // 1. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function even_or_odd(number) {
     return (number % 2 ) ? "Odd" : "Even" ;
 }
-
-  
