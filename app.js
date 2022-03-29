94f15557053fd29341a702d9b8ac607d9e46d663
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-03-29 17:59:19 +0530

    reomve spaces from a string

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
+
+// 16. Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
 // 15. Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
