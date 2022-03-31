71bd8be52d99d454c252d5bc4c47acf40c46e9a1
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-03-31 19:20:28 +0530

    keep hydrated

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 18. Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
 
 // 17. Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
