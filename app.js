c01aa3ffd1cb3c11cda38be67776b8231b56a7f3
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-02 20:28:24 +0530

    name to initials

--- app.js
+++ app.js
@@ -8,13 +8,25 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
+// 24. Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+function abbrevName(name) {
+  let nameArr = name.split(" ");
+  let initialOne = nameArr[0].slice(0, 1);
+  let initialTwo = nameArr[1].slice(0, 1);
+  return initialOne.toUpperCase() + "." + initialTwo.toUpperCase();
+}
+
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 23. Convert number to reversed array of digits
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 // Example:
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
-
 
 function digitize(n) {
   //code here
