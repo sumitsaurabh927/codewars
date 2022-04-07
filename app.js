f6396c54e268f56f88a477d057f3d0cdcb9ac152
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-07 17:48:34 +0530

    is he gonna survive?

--- app.js
+++ app.js
@@ -6,6 +6,15 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
+// 34. A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+
+function hero(bullets, dragons) {
+  //Get Coding!
+  return dragons * 2 > bullets ? false : true;
+}
+
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 // 33. Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
