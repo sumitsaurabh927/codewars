c4784dc159e3215076478fdeb572f56712c0e08d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-09 10:43:58 +0530

    reverse sequence

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
+// 37. Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let arr = [];
+  if(n){
+    for(n; n>0; n--){
+      arr.push(n);
+    }
+  }
+  return arr;
+};
+
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 36. Write a function which calculates the average of the numbers in a given list.
 
