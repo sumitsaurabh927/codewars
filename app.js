0ea6312573a88d0672f431d89ba2fdd2c7563297
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-12 12:33:52 +0530

    plays bajo

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+// 40. Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  // Implement me
+  return (name[0]==='R' || name[0] === 'r') ? `${name} plays banjo` : `${name} does not play banjo`;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
