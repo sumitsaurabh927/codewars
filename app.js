98e75734c11de0cd4987df133af187a0307627e2
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-15 22:30:19 +0530

    simple multiplication

--- app.js
+++ app.js
@@ -9,6 +9,15 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
+// 44. This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+
+function simpleMultiplication(number) {
+  // your code........
+return ( number % 2 === 0 ) ? number * 8 : number * 9;
+}
+
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
