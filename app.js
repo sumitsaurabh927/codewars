6053368b1f15eb823786b47ae1fb254c5b349ad4
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-17 23:31:56 +0530

    simple multiply function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
 
+// 47.  Write here your multiply-function
+
+const multiply = (numOne, numTwo) =>{
+  return numOne * numTwo;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 46. After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
