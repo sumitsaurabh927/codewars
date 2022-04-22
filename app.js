2fc205fb5309e477ce85d6ed276d1d1d4ca2110d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-22 20:15:28 +0530

    perimeter or area

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 54. You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
 
+// area_or_perimeter(6, 10) --> 32
+// area_or_perimeter(3, 3) --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+const areaOrPerimeter = function(l , w) {
+  return (l===w) ? l * w : 2 * ( l + w) 
+};
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 53. Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 
