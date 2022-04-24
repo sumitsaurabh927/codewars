4e886af3521a30fc3028bf1709622fc75d38700d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-24 20:26:43 +0530

    find third angle

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
+
+// 57. You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
 // 56. Sentence Smash
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 
