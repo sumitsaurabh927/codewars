889d991eb0e9d6d10c097f0d8ef787bb95012b92
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-25 16:57:56 +0530

    total points

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 62. Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  // your code here
+  let points = 0;
+  games.map(eachResult => {
+    let curScore = eachResult.split(':')
+    if (curScore[0] > curScore[1]) {
+      points += 3;
+    } else if (curScore[0] === curScore[1]) {
+      points += 1;
+    }
+
+  })
+  return points;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
 // 61. You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
 
 // As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
