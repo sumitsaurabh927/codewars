81f7a37ef715b15333e963ff6f564ce2bbb20c2b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-25 19:44:52 +0530

    rock paper scissor

--- app.js
+++ app.js
@@ -128,8 +128,24 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 65. Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
 
+// Examples:
 
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+  if ((p1 === 'scissors' && p2 === 'paper') || (p1 === 'rock' && p2 === 'scissors') || (p1 === 'paper' && p2 === 'rock')) {
+    return "Player 1 won!";
+  } else if ((p2 === 'scissors' && p1 === 'paper') || (p2 === 'rock' && p1 === 'scissors') || (p2 === 'paper' && p1 === 'rock')) {
+    return "Player 2 won!";
+  } else {
+    return "Draw!";
+  }
+};
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -146,8 +162,8 @@
 
 function countBy(x, n) {
   let z = [];
-  for(let i = 1; i<n+1; i++){
-    z.push(x*i)
+  for (let i = 1; i < n + 1; i++) {
+    z.push(x * i)
   }
   return z;
 }
