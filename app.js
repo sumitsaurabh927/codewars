446abfd4f7f149a4ffe2b62b602bf6089c0c046f
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-26 20:46:47 +0530

    traffic lights

--- app.js
+++ app.js
@@ -122,8 +122,26 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 67. You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, update_light('green') should return 'yellow'.
+
+// Note: Use "" instead of '' in C++.
 
 
+function updateLight(current) {
+  
+  if(current === 'green'){
+    return 'yellow'
+  }else if(current === 'yellow'){
+    return 'red'
+  }else if(current === 'red'){
+    return 'green'
+  }
+
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 66. Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
