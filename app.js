98f1b72a05fc24eccdfb273c116acd9f3a9f6b72
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-27 17:50:44 +0530

    total messy goals

--- app.js
+++ app.js
@@ -107,7 +107,22 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 72.
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+let laLigaGoals = 43;
+let championsLeagueGoals = 10
+let copaDelReyGoals = 5;
 
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 71. The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
