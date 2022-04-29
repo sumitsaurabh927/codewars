c534eccb5a91cc9f3eb9c2ce93f3abe061d0df26
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-29 13:54:04 +0530

    gradebook

--- level-eight/app.js
+++ level-eight/app.js
@@ -95,8 +95,32 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 76. Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
+function getGrade(s1, s2, s3) {
+  // Code here
+  let avg = (s1 + s2 + s3) / 3;
+  if (avg < 60) {
+    return 'F';
+  } else if (avg >= 60 && avg < 70) {
+    return 'D';
+  } else if (avg >= 70 && avg < 80) {
+    return 'C';
+  } else if (avg >= 80 && avg < 90) {
+    return 'B';
+  } else if (avg >= 90 && avg <= 100) {
+    return 'A';
+  }
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 75. The Story:
 // Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
