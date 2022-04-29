8d1aed3f62dc742929d3aeee986dcce78afac528
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-04-29 14:17:04 +0530

    the feast of many beasts

--- level-eight/app.js
+++ level-eight/app.js
@@ -86,10 +86,30 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 79.All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
 
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+  //your function here
+  let beastArr = beast.split('');
+  let dishArr = dish.split('');
+  return (beastArr[0] === dishArr[0] && beastArr[beastArr.length - 1] === dishArr[dishArr.length - 1]) ? true : false;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 78. You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
 
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  let answer = inputString.split('');
+  return Number(answer[0]);
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 77. Your function takes two arguments:
