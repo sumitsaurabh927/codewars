f7f48667f07b65c51ea04150e6f7657b8469a95a
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-05 20:15:34 +0530

    debug sayHello()

--- level-eight/app.js
+++ level-eight/app.js
@@ -59,8 +59,15 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 88. Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
 
+// Example output:
 
+// Hello, Mr. Spock
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 87. In this Kata we are passing a number (n) into a function.
 
