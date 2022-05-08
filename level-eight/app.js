6c6dc1034f7afb34327ca78cb33135761b9392e2
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-08 09:27:03 +0530

    remove vowels from a string

--- level-eight/app.js
+++ level-eight/app.js
@@ -38,8 +38,27 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 95. Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
 
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
 
+function shortcut(string) {
+  let stringArr = string.split('');
+  let result = [];
+  stringArr.map(x => {
+    if (!(x === 'a' || x === 'A' || x === 'e' || x === 'E' || x === 'i' || x === 'I' || x === 'o' || x === 'O' || x === 'u' || x === 'U')) {
+      result.push(x);
+    }
+
+  })
+  return result.join('');
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 94. In this kata you will create a function that takes in a list and returns a list with the reverse order.
 
