1e8593a2fc2d9de558aea8aef61a5f41902c6131
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-09 11:23:44 +0530

    Alan Partridge II - Apple Turnover

--- level-eight/app.js
+++ level-eight/app.js
@@ -32,8 +32,10 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
-
-
+// 96. Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+function apple(x) {
+  return (x * x > 1000) ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 96. Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
 
