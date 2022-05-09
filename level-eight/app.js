30de2096838421d3bad702dc6880786205dc4072
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-09 11:26:15 +0530

    How many lightsabers do you own

--- level-eight/app.js
+++ level-eight/app.js
@@ -29,8 +29,21 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 97. Inspired by the development team at Vooza, write the function that
 
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
 
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+function howManyLightsabersDoYouOwn(name) {
+  return (name === 'Zach') ? 18 : 0;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 96. Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
 function apple(x) {
