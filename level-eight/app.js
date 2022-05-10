5163c94598b0a9ed1b63364acf1f6eb302b7f30b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-10 21:42:35 +0530

    character from ASCII code

--- level-eight/app.js
+++ level-eight/app.js
@@ -26,7 +26,18 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 100. Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
 
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+function getChar(c) {
+  // ...
+  return String.fromCharCode(c);
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 97. Inspired by the development team at Vooza, write the function that
