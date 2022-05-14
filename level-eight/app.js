1304dd85b4a8025ea26126aa350513f6a38d6ae3
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-14 15:13:55 +0530

    prolog

--- level-eight/app.js
+++ level-eight/app.js
@@ -14,8 +14,19 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 103. Prolog:
+// This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
 
+// Task:
+// Write function typeValidation that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
 
+// Examples:
+// typeValidation(42, "number");   // => true
+// typeValidation("42", "number"); // => false
+function typeValidation(variable, type) {
+  // Your code should be here ;)
+  return (typeof (variable) === type);
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 102. Return the type of the sum of the two arguments
 
