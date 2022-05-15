9f95c9c8962d6a8b8e68e9d9eb3d276e32f7e689
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-15 23:16:53 +0530

    return day

--- level-eight/app.js
+++ level-eight/app.js
@@ -11,8 +11,39 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 104. Complete the function which returns the weekday according to the input number:
 
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+function whatday(num) {
 
+  // put your code here
+
+  if (num === 1) {
+    return 'Sunday';
+  } else if (num === 2) {
+    return 'Monday';
+  } else if (num === 3) {
+    return 'Tuesday';
+  } else if (num === 4) {
+    return 'Wednesday';
+  } else if (num === 5) {
+    return 'Thursday';
+  } else if (num === 6) {
+    return 'Friday';
+  } else if (num === 7) {
+    return 'Saturday';
+  } else {
+    return 'Wrong, please enter a number between 1 and 7';
+  }
+
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 103. Prolog:
 // This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
