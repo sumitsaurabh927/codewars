6c9341001254a04625e7982d70cdb81bd7147d5e
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-16 20:18:37 +0530

    return the sum of strings

--- level-eight/app.js
+++ level-eight/app.js
@@ -8,8 +8,33 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 105. Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
 
 
+function sumStr(a, b) {
+  if (a.length === 0) {
+    return (Number(b) + 0).toString();
+  } else if (b.length === 0) {
+    return (Number(a) + 0).toString();
+  } else if (a.length === 0 && b.length === 0) {
+    return '0';
+  } else {
+    return (Number(b) + Number(a)).toString();
+  }
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 104. Complete the function which returns the weekday according to the input number:
 
