70197197bb531c9aae57785efa4474d1bbfc9ee1
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-17 20:59:45 +0530

    ensure a question

--- level-eight/app.js
+++ level-eight/app.js
@@ -5,8 +5,17 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 106. Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
 
+// For example (Input --> Output)
 
+// "Yes" --> "Yes?"
+// "No?" --> "No?"
+
+function ensureQuestion(s) {
+  // Code here
+  return (s.endsWith('?')) ? s : `${s}?`;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 105. Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
 
