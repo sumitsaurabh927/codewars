509296212b8a672f8bd7fafb33acab0a787be67d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-18 15:10:12 +0530

    expressions matter

--- level-eight/app.js
+++ level-eight/app.js
@@ -1,6 +1,32 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 108. Task
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+// Consider an Example :
+// With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
 
+// 1 * (2 + 3) = 5
+// 1 * 2 * 3 = 6
+// 1 + 2 * 3 = 7
+// (1 + 2) * 3 = 9
+// So the maximum value that you can obtain is 9.
 
+// Notes
+// The numbers are always positive.
+// The numbers are in the range (1  ≤  a, b, c  ≤  10).
+// You can use the same operation more than once.
+// It's not necessary to place all the signs and brackets.
+// Repetition in numbers may occur .
+// You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+
+function expressionMatter(a, b, c) {
+  let first = a * (b + c);
+  let second = a * b * c;
+  let third = a + (b * c);
+  let fourth = (a + b) * c;
+  let fifth = a + b + c;
+  return Math.max(first, second, third, fifth, fourth);
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 107. altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 // Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
