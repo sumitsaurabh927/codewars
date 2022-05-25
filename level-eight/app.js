1374c13cc8d3713a64270176f4eefa1b167e936a
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-25 22:20:23 +0530

    multiple of index

--- level-eight/app.js
+++ level-eight/app.js
@@ -41,7 +41,26 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 116. Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+function multipleOfIndex(array) {
+  return array.filter((item,index)=> item%index===0);
+
+  // let output = [];
+  // for (let i = 1; i < array.length; i++) {
+  //   if (Math.abs(array[i] % i) === 0) {
+  //     output.push(array[i])
+  //   }
+  // }
+  // return output;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 115. In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
