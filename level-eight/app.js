302107dcb03ce73fdd0e905d6575f2516c16634b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-26 18:42:01 +0530

    find first non consecutive number

--- level-eight/app.js
+++ level-eight/app.js
@@ -38,8 +38,37 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 117. Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+// 1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
 
 
+
+function firstNonConsecutive(arr) {
+  if (arr.length < 2) {
+    return null
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] !== arr[i] + 1) {
+      return arr[i + 1];
+    } else if (i + 2 === arr.length) {
+      // we've to do i+2 because we're checking for (i+1)th item in (i)th iteration
+      return null;
+    } else if (arr.length === 0) {
+      return null;
+    }
+  }
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 116. Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
@@ -51,7 +80,7 @@
 // [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
 function multipleOfIndex(array) {
-  return array.filter((item,index)=> item%index===0);
+  return array.filter((item, index) => item % index === 0);
 
   // let output = [];
   // for (let i = 1; i < array.length; i++) {
