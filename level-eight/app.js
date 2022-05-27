f425172a28b1775d74dc97f0540a72d0960ec090
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-27 20:41:19 +0530

    grasshopper: check for factor

--- level-eight/app.js
+++ level-eight/app.js
@@ -32,8 +32,27 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 119. This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
 
 
+
+function checkForFactor (base, factor) {
+  // code here
+  return base % factor === 0 ? true : false;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 118. Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
 
