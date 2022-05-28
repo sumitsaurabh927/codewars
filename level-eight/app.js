a1366dd9eea9cff13e1cdfa362ced091bfdfd660
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-28 22:49:48 +0530

    template literal

--- level-eight/app.js
+++ level-eight/app.js
@@ -29,7 +29,19 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 120. Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+// Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
 
+let TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 119. This function should test if the factor is a factor of base.
