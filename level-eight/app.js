25bc6ac9e2aa1e7485a93b39ea6c8e402f8f1ffa
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-06-01 19:38:02 +0530

    I love you, a little, a lot, passionately, not at all

--- level-eight/app.js
+++ level-eight/app.js
@@ -17,13 +17,40 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 124. Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
 
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+  // your code
+  if (nbPetals % 6 === 1) {
+    return 'I love you'
+  } else if (nbPetals % 6 === 2) {
+    return 'a little'
+  } else if (nbPetals % 6 === 3) {
+    return 'a lot'
+  } else if (nbPetals % 6 === 4) {
+    return 'passionately'
+  } else if (nbPetals % 6 === 5) {
+    return 'madly'
+  } else if (nbPetals % 6 === 0) {
+    return 'not at all'
+  }
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 123. A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
 
 // So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
-function checkTheBucket(bucket){
+function checkTheBucket(bucket) {
   //your code here
   return bucket.includes('gold') ? true : false;
 }
