b2d5b19ba028b25b00f98cd496bf69a6684135e1
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-06-02 16:08:25 +0530

    add clever solution

--- level-eight/app.js
+++ level-eight/app.js
@@ -15,19 +15,14 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 125. Write a function that will check if two given characters are the same case.
-
 // If either of the characters is not a letter, return -1
 // If both characters are the same case, return 1
 // If both characters are letters, but not the same case, return 0
 // Examples
 // 'a' and 'g' returns 1
-
 // 'A' and 'C' returns 1
-
 // 'b' and 'G' returns 0
-
 // 'B' and 'g' returns 0
-
 // '0' and '?' returns -1
 
 function sameCase(a, b) {
@@ -53,6 +48,20 @@ function sameCase(a, b) {
     return -1;
   }
 }
+
+// clever solution
+function sameCase(a, b){
+//   const alpha = 'abcdefghijklmnopqrstuvwxyz'
+//   if(!alpha.includes(a.toLowerCase()) || !alpha.includes(b.toLowerCase())) return -1
+
+//   const checkCase = x => {
+//     if(x === x.toLowerCase()) return 'lower'
+//     if(x === x.toUpperCase()) return 'upper'
+//   }
+
+//   return checkCase(a) === checkCase(b) ? 1 : 0
+// }
+
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 124. Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
 
