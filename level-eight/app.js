1546529e1121197c3878f996f08a5829ac8e2f25
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-06-05 23:15:11 +0530

    added previous question

--- level-eight/app.js
+++ level-eight/app.js
@@ -19,7 +19,13 @@ function getSize(width, height, depth) {
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 127. Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
 function between(a, b) {
   // your code here
   let arr = [];
