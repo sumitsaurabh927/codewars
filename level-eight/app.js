dd438b4793c525e5a7003022a6b1ed575440c90d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-06-09 23:22:31 +0530

    swap array values

--- level-eight/app.js
+++ level-eight/app.js
@@ -176,8 +176,18 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 132. I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
 
-
+// Can you figure out what's wrong here?
+function swapValues(args) {
+  let temp = args[0];
+  args[0] = args[1];
+  args[1] = temp;
+  let newArr = [];
+  newArr.push(args[1]);
+  newArr.push(args[0]);
+  return newArr;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 // 131. add the value "codewars" to the websites array 1,000 times
