da4be06f988b2a6ba5b0428fa66a67c76590158b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-06-25 22:56:41 +0530

    date age range

--- level-eight/app.js
+++ level-eight/app.js
@@ -128,8 +128,22 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 148. Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). It's the 'recommended' age range in which to date someone.
 
 
+// minimum age <= your age <= maximum age #Task
+
+// Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+
+// This equation doesn't work when the age <= 14, so use this equation instead:
+
+// min = age - 0.10 * age
+// max = age + 0.10 * age
+// You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+function datingRange(age) {
+  //return min-max
+  return age > 14 ? (`${Math.floor(age / 2 + 7)}-${Math.floor((age - 7) * 2)}`) : (`${Math.floor(age - (0.10 * age))}-${Math.floor(age + (0.10 * age))}`)
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 147. You have to write a function that describe Leo:
 
