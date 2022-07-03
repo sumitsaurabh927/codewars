c7b8df0efe51a32b326caa73f9dc8d448a45cf32
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-03 15:19:44 +0530

    mistakes of a character recognition software

--- level-eight/app.js
+++ level-eight/app.js
@@ -104,8 +104,30 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 156. Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
 
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
 
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+function correct(string) {
+  let split = string.split('');
+
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] === '5') {
+      split[i] = 'S';
+    } else if (split[i] === '0') {
+      split[i] = 'O';
+    } else if (split[i] === '1') {
+      split[i] = 'I';
+    }
+  }
+  return split.join('');
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 155. There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
 
