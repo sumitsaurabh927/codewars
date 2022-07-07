f31fdf069e5d8f58151c513fa16d7c8bebafddf9
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-07 21:53:55 +0530

    did she say hello?

--- level-eight/app.js
+++ level-eight/app.js
@@ -92,8 +92,22 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 158. You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
 
+// Write a simple function to check if the string contains the word hallo in different languages.
 
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+function validateHello(greetings) {
+  return greetings.toLowerCase().includes('hello') || greetings.toLowerCase().includes('ciao') || greetings.toLowerCase().includes('salut') || greetings.toLowerCase().includes('hallo') || greetings.toLowerCase().includes('hola') || greetings.toLowerCase().includes('ahoj') || greetings.toLowerCase().includes('czesc');
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 159. You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
 
