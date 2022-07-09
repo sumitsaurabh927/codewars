a65444d366ccea37eeaf265c23b618aa4dcdc841
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-09 22:38:48 +0530

    for loop

--- level-eight/app.js
+++ level-eight/app.js
@@ -86,8 +86,22 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 160. Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even.
+//I've defined two array odd and even in the function, and also wrote the return statement. your work is write a for loop.
 
 
+function pickIt(arr) {
+  let odd = [], even = [];
+  //coding here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i])
+    } else {
+      odd.push(arr[i])
+    }
+  }
+  return [odd, even];
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 159. There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
 
