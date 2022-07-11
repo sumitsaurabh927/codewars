d00d6db8f439989c95142dc7a30a83b15e5d46c3
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-11 23:33:29 +0530

    grader

--- level-eight/app.js
+++ level-eight/app.js
@@ -80,8 +80,37 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 162. Create a function that takes a number as an argument and returns a grade based on that number.
 
+// Score	Grade
+// Anything greater than 1 or less than 0.6	"F"
+// 0.9 or greater	"A"
+// 0.8 or greater	"B"
+// 0.7 or greater	"C"
+// 0.6 or greater	"D"
+// Examples:
+// grader(0)   should be "F"
+// grader(1.1) should be "F"
+// grader(0.9) should be "A"
+// grader(0.8) should be "B"
+// grader(0.7) should be "C"
+// grader(0.6) should be "D"
 
+function grader(score) {
+  if (score < 0.6) {
+    return 'F';
+  } else if (score < 0.7) {
+    return 'D';
+  } else if (score < 0.8) {
+    return 'C';
+  } else if (score < 0.9) {
+    return 'B';
+  } else if (score <= 1) {
+    return 'A';
+  } else if (score > 1) {
+    return 'F';
+  }
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 161. You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
 
