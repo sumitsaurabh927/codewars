7cf7224175bf3b228cc0c1ed29cbeccd805160e8
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-14 22:23:50 +0530

    break strict comparison

--- level-eight/app.js
+++ level-eight/app.js
@@ -71,8 +71,17 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 165. In JavaScript, there is a special case where strict comparison of the same variable returns false! Try to find out what must be done to get such result!
+
+// var x = something;
+// x === x // returns false!
+// Write a function which will return value for which strict comparison will give false!
 
 
+function findStrangeValue() {
+  // your code!
+  return NaN;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 164. Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
 
