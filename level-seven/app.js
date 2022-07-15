d2c6aa3a76f45fa4c5818574cead6c863368673b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-15 22:43:28 +0530

    highest and lowest

--- level-seven/app.js
+++ level-seven/app.js
@@ -32,8 +32,32 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 4. In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
-
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+function highAndLow(numbers) {
+        // ...
+        let array = numbers.split(' ')
+        let max = array[0];
+        let min = array[0];
+        if (array.length > 1) {
+                for (let i = 0; i < array.length; i++) {
+                        if (Number(array[i]) > max) {
+                                max = Number(array[i]);
+                        } else if (Number(array[i]) < min) {
+                                min = Number(array[i]);
+                        }
+                }
+        } else return `${Number(array[0])} ${Number(array[0])}`
+        return `${max} ${min}`;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 3. Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
