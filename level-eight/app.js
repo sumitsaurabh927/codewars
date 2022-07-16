ebfbf719628719ab7ee7d8f839a889536c207fe8
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-16 22:21:29 +0530

    powers of 2

--- level-eight/app.js
+++ level-eight/app.js
@@ -68,8 +68,19 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 166. Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
 
-
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i < n + 1; i++) {
+    result.push(Math.pow(2, i))
+  }
+  return result
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 165. In JavaScript, there is a special case where strict comparison of the same variable returns false! Try to find out what must be done to get such result!
 
