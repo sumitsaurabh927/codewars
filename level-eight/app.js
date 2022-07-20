dfe68b7686256bf1fea5f3fc6b3bd0444d8ef4ad
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-07-20 07:15:34 +0530

    wall of ideas

--- level-eight/app.js
+++ level-eight/app.js
@@ -56,8 +56,24 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 170. For every good kata idea there seem to be quite a few bad ones!
+
+//In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 
+function well(x) {
+  let count = 0;
+  for (let k of x) {
+    if (k === 'good') {
+      count++;
+    }
+  }
+  if (count < 1) {
+    return 'Fail!'
+  } else if (count < 3) {
+    return 'Publish!'
+  } else return 'I smell a series!'
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 169. Write a function that returns a string in which firstname is swapped with last name.
 
