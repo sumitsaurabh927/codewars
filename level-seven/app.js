a30a2d40a247ed7426bb784a786459c639ec5e80
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-07-22 21:43:02 +0530

    middle character

--- level-seven/app.js
+++ level-seven/app.js
@@ -30,13 +30,21 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
-
+function getMiddle(s) {
+        if (s.length % 2 === 0) {
+                return `${s[s.length / 2 - 1]}${s[s.length / 2]}`;
+        } else return `${s[Math.floor(s.length / 2)]}`;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 4. In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 // Examples
 // highAndLow("1 2 3 4 5");  // return "5 1"
-// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"function getMiddle(s) {
+        if (s.length % 2 === 0) {
+                return `${s[s.length / 2 - 1]}${s[s.length / 2]}`;
+        } else return `${s[Math.floor(s.length / 2)]}`;
+}
 // highAndLow("1 9 3 4 -5"); // return "9 -5"
 // Notes
 // All numbers are valid Int32, no need to validate them.
