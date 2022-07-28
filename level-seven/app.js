dc4e33cf744e2db53ca389dc4628cf9695bcedf6
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-07-28 22:24:08 +0530

    special array ops

--- level-seven/app.js
+++ level-seven/app.js
@@ -17,8 +17,35 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 9. Write a function loopArr that loops array in a specified direction by some number of steps.
 
+// By "looping array" it means removing elements from start and adding them to end of array one-by-one (if direction is "left") or removing from end and adding them to start one by-one (if direction is "right").
 
+// Function should accept three arguments:
+
+// array - non-empty array of elements of any type;
+// direction - 'left' or 'right' - tells how to loop array;
+// steps - number of steps to loop array (less or equal to array size);
+// Examples:
+
+// loopArr([1, 5, 87, 45, 8, 8], 'left', 2);
+// should produce result: [87, 45, 8, 8, 1, 5]
+
+// loopArr([1, 5, 87, 45, 8, 8], 'right', 2);
+// should produce result: [8, 8, 1, 5, 87, 45]
+
+function loopArr(arr, direction, steps) {
+        for (let i = 0; i < steps; i++) {
+                if (direction === 'left') {
+                        let item = arr.shift();
+                        arr.push(item);
+                } else if (direction === 'right') {
+                        let item = arr.pop();
+                        arr.unshift(item);
+                }
+        }
+        return arr;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 8. Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 
