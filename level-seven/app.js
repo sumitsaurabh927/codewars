c613526af65bec40ed772d62c2067c463fd1a0c3
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-07-29 18:55:25 +0530

    added questions for the next two days

--- level-seven/app.js
+++ level-seven/app.js
@@ -11,9 +11,35 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 11. Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+
+// 10. Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// // If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
