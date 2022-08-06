40b7b3d8685b506b87ee253770680cd11e6aeac3
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-06 12:18:14 +0530

    digits of a number to the power of 5

--- level-eight/app.js
+++ level-eight/app.js
@@ -32,8 +32,21 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 178. Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
 
 
+function multiply(number) {
+  //your code here
+  let abs = number > -1 ? number : -1 * number;
+  let digits = abs.toString().length;
+  return number * Math.pow(5, digits);
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 177. In javascript, Array is one of basic data types. Define an empty array can use var arr=new Array() or var arr=[]
 
