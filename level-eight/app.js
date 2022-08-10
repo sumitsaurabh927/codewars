206e6a48853478413527346176e72705da78f753
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-10 12:09:52 +0530

    take the derivative

--- level-eight/app.js
+++ level-eight/app.js
@@ -35,6 +35,15 @@
 function derive(coefficient, exponent) {
   return `${coefficient * exponent}x^${exponent - 1}`;
 }
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+// For example:
+
+// derive(7, 8)
+// In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+// derive(7, 8) --> this should output "56x^7" 
+// derive(5, 9) --> this should output "45x^8" 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 181. Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
