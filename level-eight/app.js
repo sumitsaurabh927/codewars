d8e521b189f150fabb0ec4ac259c60322b119fe6
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-11 20:44:52 +0530

    ascii code of char

--- level-eight/app.js
+++ level-eight/app.js
@@ -17,8 +17,13 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 183. Get ASCII value of a character.
 
+// For the ASCII table you can refer to http://www.asciitable.com/
 
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 182. This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
 
