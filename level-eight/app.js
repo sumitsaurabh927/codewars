aea101ba8385592a8dfa8230cd245cb7a3eaf745
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-12 22:37:55 +0530

    hello kitty and barbie doll

--- level-eight/app.js
+++ level-eight/app.js
@@ -14,8 +14,30 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 184. Coding in function grabDoll. function accept 1 parameter:dolls. it's a string array, a list of some dolls.
 
+// You need traverse dolls by using for loop. If element is "Hello Kitty" or "Barbie doll", you should push it to a bag(bag is an array, I've defined in the function); if it's other strings, we should use continue skip it.
 
+// When the bag has three element, bag is full. You should use break jump out the loop; If bag is not full, you should traverse dolls until the last element.
+
+// Return the bag after for loop finished.
+
+// You should use for, break and continue in your code. otherwise, your solution may not pass this kata.
+
+function grabDoll(dolls) {
+  let bag = [];
+  //coding here
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      if (bag.length === 3) {
+        break;
+      } else {
+        bag.push(dolls[i]);
+      }
+    } continue;
+  }
+  return bag;
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 183. Get ASCII value of a character.
 
