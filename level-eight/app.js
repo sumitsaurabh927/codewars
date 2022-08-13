5d4456afde44b685d5fe6ffd60388a0da6b7eba9
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-13 20:18:26 +0530

    replacing a string

--- level-eight/app.js
+++ level-eight/app.js
@@ -11,8 +11,28 @@
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 185. An AI has infected a text with a character!!
 
+// This text is now fully mutated to this character.
 
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+function contamination(text, char) {
+  // Code here ;)
+  let arr = text.split('');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = char;
+  }
+  return arr.join('');
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 184. Coding in function grabDoll. function accept 1 parameter:dolls. it's a string array, a list of some dolls.
 
