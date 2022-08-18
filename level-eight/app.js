8d908c63ea07200a57e10835bbd5874dc28542db
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-18 23:12:39 +0530

    padding a string

--- level-eight/app.js
+++ level-eight/app.js
@@ -1,3 +1,21 @@
+// 190. You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+// Finally, return the padded string.
+
+function padIt(str, n) {
+  //coding here
+  let i = 0
+  while (i < n) {
+    if (i % 2 === 0) {
+      str = `*${str}`;
+    } else if (i % 2 === 1) {
+      str = `${str}*`;
+    }
+    i++;
+  }
+  return str;
+}
+
 // -------------------------------------------------------------------------------------------------------------------------------------
 // 189. Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 
