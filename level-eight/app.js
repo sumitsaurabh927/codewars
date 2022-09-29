4c3a09e6bef958facfe1bd7892bd824513ac376a
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-09-29 15:26:22 +0530

    grasshopper: order of operations

--- level-eight/app.js
+++ level-eight/app.js
@@ -25,7 +25,12 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------------------------------------
+// 196. Grasshopper Order of Operations
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
 
+function orderOperations() {
+  return ((2 + 2) * (2 + 2)) * 2
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 // 195. Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
